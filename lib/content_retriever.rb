c91b978e6eaf66de675ef6c6eb6e6b3f34c94d12
inmusic <info@in-music.com.au>
2014-04-05 01:54:09 +1100

    Update content_retriever.rb

--- lib/content_retriever.rb
+++ lib/content_retriever.rb
@@ -6,10 +6,10 @@ class ContentRetriver
 		{
 			total: 1,
 			contents: [{
-				title: '简单几步成为微信公众平台开发者',
-				description: '该公众账户是为了演示如何成为微信平台的开发者，如果想要了解更详细的内容，请点击该消息。',
-				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=3097029212&r=975188&token=1685872554',
-				url: 'http://linkedme.in/2013/09/25/turoial-to-be-wechat-public-account-developer/'}]
+				title: '田馥甄與林宥嘉 Love In Music Concert',
+				description: '华语女子天团SHE的HEBE，田馥甄以爱唱歌的女孩的身份与华语新一代小歌王林宥嘉会在五月为澳洲乐迷带来惊喜。二人将全球首度合作，分别在五月二日及五月五日踏上墨尔本会议及展览中心(MCEC) 和悉尼Horden Pavilion的舞台，献上独一无二的音乐会，传递爱的正能量。',
+				picture: 'http://www.in-music.com.au/inmusic/HebeYogaPoster.jpg',
+				url: 'http://www.in-music.com.au/weixin/'}]
 		}
 	end
 end
